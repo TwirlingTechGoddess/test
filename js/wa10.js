cfda893f60ac37e5a8d5b5b5e225a9d5cf2481c9
LeeLee James <leja9931@colorado.edu>
2023-11-02 14:59:08 -0600

    Add upgrades

--- js/wa10.js
+++ js/wa10.js
@@ -21,18 +21,26 @@ const insertY = ["the soup kitchen",
 const insertZ = ["spontaneously combusted",
 "melted into a puddle on the sidewalk",
 "turned into a slug and crawled away"]
-const yItem = randomValueFromArray(insertY);
-const zItem = randomValueFromArray(insertZ);
-const xItem = randomValueFromArray(insertX);
-const storyText = `It was 94 fahrenheit outside, so ${xItem} went for a walk. When they got to ${yItem}, they stared in horror for a few moments, then ${zItem}. Bob saw the whole thing, but was not surprised — ${xItem} weighs 300 pounds, and it was a hot day.`
+
 
 // 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
-randomize.addEventListener('click', result);
+randomize.addEventListener('click', function () {
+    if(customName.value){
+        result();
+    }
+}
+);
 //add conditional based on if the textbox has value
 
 function result() {
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+    const xItem = randomValueFromArray(insertX);
+    const storyText = `It was 94 fahrenheit outside, so ${xItem} went for a walk. When they got to ${yItem}, they stared in horror for a few moments, then ${zItem}. Bob saw the whole thing, but was not surprised — ${xItem} weighs 300 pounds, and it was a hot day.`
     let newStory = storyText;
+// to make it dynamic on each button click I had to move variables into the function itself
+
     if(customName.value !== '') {
         const name = customName.value;
         newStory = newStory.replace(/Bob/g, name);
