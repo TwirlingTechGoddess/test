ab620396b0785a88a73489635dcb91b7214bd58f
LeeLee James <leja9931@colorado.edu>
2023-11-07 10:16:51 -0700

    Start html and js files and  structure for ica11

---
+++ js/ica/ica11.js
@@ -0,0 +1,14 @@
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
+
+function draw() {
+    // concole.log("TEST");
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    for(let i = 0; i< 100; i++){
+
+    }
+    
+}
+
+BigInt.addEventListener("click", draw);
