972ebfc232e97b3243c8d2f74514ef1f28c0798c
LeeLee James <leja9931@colorado.edu>
2023-11-09 15:03:45 -0700

    Implement darken/lighten functionality

--- js/wa/wa11.js
+++ js/wa/wa11.js
@@ -24,8 +24,21 @@ picArray.forEach( pic => {
 
 // displayedImage.addEventListener('click')
 thumbBar.addEventListener('click', function(event){
-    displayedImage.setAttribute('src', (event.target.getAttribute("src")));
-    displayedImage.setAttribute('alt', (event.target.getAttribute("alt")));
+    displayedImage.setAttribute('src', (event.target.getAttribute('src')));
+    displayedImage.setAttribute('alt', (event.target.getAttribute('alt')));
 });
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', function(){
+    if(btn.getAttribute('class') === 'dark'){
+
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    };
+});
