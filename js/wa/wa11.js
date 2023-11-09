b3713b8e7220eb7d9872faaaee7789ae3f4c5485
LeeLee James <leja9931@colorado.edu>
2023-11-09 13:20:36 -0700

    Dynamically add clicked image to displayed image

--- js/wa/wa11.js
+++ js/wa/wa11.js
@@ -26,7 +26,6 @@ picArray.forEach( pic => {
 thumbBar.addEventListener('click', function(event){
     displayedImage.setAttribute('src', (event.target.getAttribute("src")));
     displayedImage.setAttribute('alt', (event.target.getAttribute("alt")));
-
 });
 
 /* Wiring up the Darken/Lighten button */
