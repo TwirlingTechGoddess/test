8400e0b6bec30f4d56cc55376102b051564e3c62
LeeLee James <leja9931@colorado.edu>
2023-11-26 18:35:02 -0700

    Commit final updates

--- js/wa/wa12.js
+++ js/wa/wa12.js
@@ -1,3 +1,4 @@
+let url = 'https://api.pexels.com/v1/search?query=dad&per_page=1';
 const questionBtn = document.querySelector('#js-new-quote');
 const answerBtn = document.querySelector('#js-new-answer');
 const questionTxt = document.querySelector('#js-quote-text');
@@ -7,7 +8,30 @@ let answer = '';
 questionBtn.addEventListener('click', getQuote);
 answerBtn.addEventListener('click', displayAnswer);
 
+ function generateImage(){
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            authorization: 'a2ULNxz4rOvrqxMiDP2Hx26OBcwFkDXDHFPZjJvBN31WqxCCTZvUlU5S'
+        }
+    };
+    try {
+        fetch(url, options)
+            .then((response) => response.json())
+            .then((data) => {
+                const bckgrndImg = data.photos[0].src.large2x 
+                document.body.style.backgroundImage = `url(${bckgrndImg})`;
+                url = data.next_page;
+            })
+    } catch (error) {
+          console.error(`Download error: ${error.message}`);
+          alert('PIC FAILURE!!!')
+    }
+}
+
 async function getQuote(event){
+    generateImage();
     const url = 'https://dad-jokes.p.rapidapi.com/random/joke';
     const options = {
       method: 'GET',
@@ -15,17 +39,16 @@ async function getQuote(event){
         'X-RapidAPI-Key': 'bed3d3238cmsheab41b22ad6a8eep194fc3jsnca119823eea9',
         'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
       }
-    };
+    };    
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        console.log("Download complete", result.body);
         answerTxt.textContent = '';
         displayQuote(result.body[0]['setup']);
         answer = result.body[0]['punchline'];
       } catch (error) {
         console.error(`Download error: ${error.message}`);
-        alert('YOU ARE A FAILURE!!!')
+        alert('JOKE FAILURE!!!')
       }
 }
 
