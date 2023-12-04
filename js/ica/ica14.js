c20c359cb0cc2cc7d668fc6b9853b6c2ff9d2826
LeeLee James <leja9931@colorado.edu>
2023-12-03 23:14:43 -0700

    Layout javascript functions

--- js/ica/ica14.js
+++ js/ica/ica14.js
@@ -1,4 +1,41 @@
+// Create JSON for each employee
 
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+function employeeJSON() {
+    fetch('ica14.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
 
+// Create JSON for the company
+
+function companyJSON() {
+
+}
+
+
+// A new employee has joined the company
+
+function addEmployee() {
+    
+}
+
+
+// Calculate total salary for employees
+
+function calculateSalary() {
+
+}
+
+
+// Update salary for each raised eligible
+
+function updateSalary() {
+
+}
+
+
+// Add a property called 'wfh' to employees
+
+function addProperty() {
+    
+}
