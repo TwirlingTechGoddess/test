e77278a15cca78906eb1e9059134326560ed1be9
LeeLee James <leja9931@colorado.edu>
2023-12-04 21:58:10 -0700

    Refactor for Problem 3

--- js/wa/wa13.js
+++ js/wa/wa13.js
@@ -3,17 +3,19 @@ let employees = [
     {"firstname": "Sam", "department": "Tech", "designation": "Manager", "salary": 40000, "raise eligible": true},
     {"firstname": "Mary", "department": "Finance", "designation": "Trainee", "salary": 18500, "raise eligible": true},
     {"firstname": "Bill", "department": "HR", "designation": "Executive", "salary": 21200, "raise eligible": false}
-]
+];
 
 let company =  [
     {"companyName": "Tech Stars", "website": "www.techstars.site", "employees": ["Sam", "Mary", "Bill"]}
-]
+];
+
+let newEmployee = {"firstname": "Anna", "department": "Tech", "designation": "Executive", "salary": 25600, "raise eligible": false};
 
 
 // Create JSON for each employee
 
 function  employeeJSON() {
-    console.log("Problem 1: ")
+    console.log("Problem 1: ");
     employees.forEach(employee => {
         console.log(employee);
     })
@@ -22,7 +24,7 @@ function  employeeJSON() {
 // Create JSON for the company
 
 function companyJSON() {
-    console.log("Problem 2: ")
+    console.log("Problem 2: ");
     employees.forEach(company => {
         console.log(company);
     })
@@ -31,15 +33,20 @@ function companyJSON() {
 
 // A new employee has joined the company
 
-function addEmployee() {
-    
+function addEmployee(newEmployee) {
+    console.log("Problem 3: ");
+    employees.push(newEmployee);
+    company.employees.push(newEmployee.firstname);
+    employees.forEach(employee => {
+        console.log(employee);
+    })
 }
 
 
 // Calculate total salary for employees
 
 function calculateSalary() {
-
+    console.log();
 }
 
 
