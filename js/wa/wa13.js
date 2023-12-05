b3c3ae9ea6446a9645de79ce74a214df1550c43e
LeeLee James <leja9931@colorado.edu>
2023-12-04 21:50:02 -0700

    Refactor for Problrm 1

--- js/wa/wa13.js
+++ js/wa/wa13.js
@@ -1,9 +1,22 @@
+// 
+let employees = [
+    {"firstname": "Sam", "department": "Tech", "designation": "Manager", "salary": 40000, "raise eligible": true},
+    {"firstname": "Mary", "department": "Finance", "designation": "Trainee", "salary": 18500, "raise eligible": true},
+    {"firstname": "Bill", "department": "HR", "designation": "Executive", "salary": 21200, "raise eligible": false}
+]
+
+let company =  [
+    {"companyName": "Tech Stars", "website": "www.techstars.site", "employees": ["Sam", "Mary", "Bill"]}
+]
+
+
 // Create JSON for each employee
 
-async function  employeeJSON() {
-    const response = await fetch('wa13.json');
-    const result = await response.json();
-    console.log(result);
+function  employeeJSON() {
+    console.log("Problem 1: ")
+    employees.forEach(employee => {
+        console.log(employee);
+    })
 }
 
 // Create JSON for the company
